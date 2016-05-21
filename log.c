4da79e435712d83bda0c2e1fe3423379e0b7a445
ruansheng <825307513@qq.com>
2016-05-21 22:14:31 +0800

    log

--- log.c
+++ log.c
@@ -10,5 +10,5 @@ void logger(int line, const char *level, const char *msg) {
     p = localtime(&timep);
     char time[20];
     sprintf(time, "%d-%d-%d %d:%d:%d", (p->tm_year + 1900), (p->tm_mon + 1), p->tm_mday, p->tm_hour, p->tm_min, p->tm_sec);
-    printf("[%s] line %d [%d]:%s", time, level, line, msg);
+    printf("[%s] line %d [%s]:%s\n", time, line, level, msg);
 }
