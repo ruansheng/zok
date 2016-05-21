a9227e509ae5f309eeb7ea8fe0022b4b608ba6af
ruansheng <825307513@qq.com>
2016-05-21 11:46:44 +0800

    add epoll

--- test/z_epoll-test.c
+++ test/z_epoll-test.c
@@ -4,9 +4,10 @@
 
 #include "../z_epoll.h"
 
-int main() {
+int main(int argc, char *argv[]) {
     int listenfd = socket_create();
-    socket_bind(listenfd, "127.0.0.1", 9527);
+    int port = atoi(argv[1]);
+    socket_bind(listenfd, "127.0.0.1", port);
     socket_listen(listenfd);
     do_event(listenfd);
 }
