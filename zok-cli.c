f30761431b3f94c38799915c427a25986c0180d5
ruansheng <825307513@qq.com>
2016-05-24 23:52:23 +0800

    cli

--- zok-cli.c
+++ zok-cli.c
@@ -5,22 +5,27 @@
 #include<stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 #include "version.h"
 
 typedef struct client{
     char *host;
     int ip;
-    int socket;
+    int sock;
+    char *line;
+    char *cmd;
 } client;
 
 int socket_create() {
-    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(listenfd == -1) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock == -1) {
         perror("socket error.\n");
         exit(0);
     }
-    return listenfd;
+    return sock;
 }
 
 void socket_connect(client *c) {
@@ -35,14 +40,14 @@ void socket_connect(client *c) {
     }
 }
 
-char *socket_read(client *c) {
+void socket_read(client *c) {
     char buf[1024];
     int s = recv(c->socket, buf, 1024, 0);
     if(s != -1) {
         perror("Mismatch in number of sent bytes");
         exit(1);
     }
-    return buf;
+    printf("%s\n", buf);
 }
 
 void socket_send(client c, char *msg) {
@@ -53,13 +58,101 @@ void socket_send(client c, char *msg) {
     }
 }
 
+void close_sock(client *c) {
+    close(c->sock);
+    exit(0);
+}
+
+/**
+ * splitString
+ */
+void splitString(client *clio) {
+    const char *c = clio->line;
+    char cmd[3][1024];
+    int i = 0;
+    int j = 0;
+    int total = 0;
+    while(*c != '\0') {
+        if(*c == ' ') {
+            cmd[i][j] = '\0';
+            i++;
+            j = 0;
+            c++;
+            while(*c == ' ') {
+                *c++;
+            }
+        } else {
+            cmd[i][j] = *c;
+            j++;
+            c++;
+            total++;
+        }
+    }
+
+    char *str = (char *)malloc(total + 9); // *queue \n set \n name \n ruansheng
+    if(str == NULL) {
+        printf("malloc error\n");
+        exit(1);
+    }
+    char *tmp = str;
+    strcpy(str, "*queue\n");
+    str += 7;
+    for(int s = 0; s <= i; s++) {
+        strcpy(str, cmd[s]);
+        str += strlen(cmd[s]);
+        strcpy(str, "\n");
+        str += 1;
+    }
+    clio->cmd = tmp;
+}
+
+/**
+ * sendCommand
+ */
+void sendCommand(client *clio) {
+    write(clio->sock, clio->cmd, strlen(clio->cmd) + 1);
+}
+
+/**
+* 获取输入的command
+* command length <= 100
+*/
+void getLineKey(client *clio) {
+    printf("%s:%d>", clio->host, clio->port);
+    char line[1024];
+    memset(line, '\0', 1014);
+    fgets(line, 1024, stdin);
+    clio->line = (char *)malloc(strlen(line));
+    strcpy(clio->line, line);
+
+    splitString(clio);
+    sendCommand(clio);
+}
+
+/**
+ * getResponse
+ */
+void getResponse(client *clio) {
+    char buffer[1024];
+    memset(buffer,0,sizeof(buffer));
+    read(clio->sock, buffer, 1024);
+    printf("%s \n", buffer);
+}
+
+
 client cli;
 
 int main(int argc, char *argv[]) {
     cli.host = "127.0.0.1";
     cli.ip = 10032;
 
-    cli.socket = socket_create();
+    cli.sock = socket_create();
     socket_connect(&cli);
+
+    for(;;) {
+        getLineKey(clio);
+        sendCommand(clio);
+        getResponse(clio);
+    }
     return 0;
 }
