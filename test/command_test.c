73266520709a72b500a7142c0cb5d0041c9ae257
ruansheng <825307513@qq.com>
2016-05-28 14:47:01 +0800

    command

--- test/command_test.c
+++ test/command_test.c
@@ -10,11 +10,14 @@ int main(int argc, char *argv[]) {
 
     int i;
     for(i = 0; i < 10000; i++) {
-        char *b[] = {
-           "set",
-           "name"
-        };
-        sprintf(b[2], i);
+        char *b1 = "set";
+        char *b2 = "name";
+        char *b3;
+        sprintf(b3, i);
+        char *b[];
+        char *b[0] = b1;
+        char *b[1] = b2;
+        char *b[2] = b3;
         zokCommandArgvToString(&zc, a, b);
         printf("%s", zc.obuf);
     }
