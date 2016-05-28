68b90fc37bb5ebb98d9e7d92a2f96cd7267f9521
ruansheng <825307513@qq.com>
2016-05-28 14:49:41 +0800

    command

--- test/command_test.c
+++ test/command_test.c
@@ -15,9 +15,9 @@ int main(int argc, char *argv[]) {
         char *b3;
         sprintf(b3, i);
         char *b[3];
-        char *b[0] = b1;
-        char *b[1] = b2;
-        char *b[2] = b3;
+        b[0] = b1;
+        b[1] = b2;
+        b[2] = b3;
         zokCommandArgvToString(&zc, a, b);
         printf("%s", zc.obuf);
     }
